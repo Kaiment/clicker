5866ccba6097473c011811333aaa669e285fe060
Unknown <kai.bedene@gmail.com>
2019-04-03 11:07:37 +0200

    Base done, clicking, buying, producing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Work from './components/Work/Work';
 import './App.css';
 
-class App extends Component {
+interface IState {
+  neuron_count: number;
+  neuron_prod: number;
+  work_count: number;
+}
+
+class App extends Component<{}, IState> {
+  constructor(props: any) {
+    super(props);
+    this.add_neuron = this.add_neuron.bind(this);
+  };
+
+  public readonly state: Readonly<IState> = {
+    neuron_count: 0,
+    neuron_prod: 0,
+    work_count: 0,
+  };
+
+  private add_neuron() {
+    this.setState(state => ({
+      neuron_count: state.neuron_count + 1,
+    }))
+  };
+
+  private add_work(price: number) {
+    this.setState(state => ({
+      work_count: state.work_count + 1,
+      neuron_count: state.neuron_count - price,
+      neuron_prod: this.state.neuron_prod + 1,
+    }))
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState(state => ({
+        neuron_count: state.neuron_count + state.neuron_prod,
+      }))
+    }, 1000);
+  }
+
   render() {
+    const { neuron_count, neuron_prod, work_count } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>
+        <div>
+          <div>{ neuron_count } neurons</div>
+          <div>{ neuron_prod } per second</div>
+          <h1 onClick={ this.add_neuron }>BRAIN</h1>
+        </div>
+        <div>
+          <Work neuron_count={ neuron_count } work_count={ work_count } add_work={ this.add_work.bind(this) } />
+        </div>
+      </div>
       </div>
     );
   }
